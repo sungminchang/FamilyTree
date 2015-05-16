df8d2ab35aaadd70a8cd07e2d523d5da49b8ccc1
SungMin <4sungmin@gmail.com>
2015-05-16 16:08:05 -0700

    Add basic functionality that trees have. Start working on implementing coding challenge requirements.

--- familyTree.js
+++ familyTree.js
@@ -1,3 +1,26 @@
 var FamilyTree = function(name) {
   this.name = name;
+  this.children = [];
+};
+
+FamilyTree.prototype.addChild = function(tree) {
+  this.children.push(tree);
+};
+
+FamilyTree.prototype.find = function(name) {
+  var length = this.children.length;
+
+  for (var i = 0; i < length; i++) {
+    var child = this.children[i];
+    if (child.name === name) {
+      return child;
+    }
+
+    var childsResult = child.find(name);
+    if (childsResult) {
+      return childsResult;
+    };
+  }
+
+  return null;
 };
--- tests.js
+++ tests.js
@@ -1,16 +1,49 @@
 var expect = chai.expect;
 var should = chai.should();
 var assert = chai.assert;
+
 var mandy = new FamilyTree('Mandy');
+var nara = new FamilyTree('Nara');
+var natalie = new FamilyTree('Natalie');
+var oliver = new FamilyTree('Oliver');
+var oscar = new FamilyTree('Oscar');
+
+mandy.addChild(natalie);
+mandy.addChild(nara);
+
+nara.addChild(oliver);
+nara.addChild(oscar);
+
 
 describe('the family tree data structure', function() {
 
-  beforeEach(function() {
-    mandy.children = [];
+  describe('basic tree functionality', function() {
+
+    it('should allow you to set the name', function() {
+      expect(mandy.name).to.equal('Mandy');
+    });
+
+    it('should maintain a collection of children', function() {
+      mandy.children.should.be.a('array');
+    });
+
+    it('should allow you to add children', function() {
+      assert(mandy.children.length === 2, 'added a child');
+    });
+
+    it('should let you find a child', function() {
+      expect(mandy.find('Nara')).to.equal(nara);
+    });
+
+    it('should let you traverse a nodes entire ancestry', function() {
+      mandy.find('Oliver').should.equal(oliver);
+      expect(mandy.find('Harold')).to.equal(null);
+    });
+
   });
 
-  it('should allow you to set the name', function() {
-    expect(mandy.name).to.equal('Mandy');
-  });
+  // describe('it should let you find the grandparent of a node', function() {
+  //   expect(mandy.)
+  // });
 
 });
