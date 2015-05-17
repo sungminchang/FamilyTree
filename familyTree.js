96f790042246a59b1acc166d268671b69f2a05e0
SungMin <4sungmin@gmail.com>
2015-05-16 17:46:53 -0700

    [Features] 1. Add method to find people with no siblings. 2. Add method to find people with no children.

--- familyTree.js
+++ familyTree.js
@@ -7,20 +7,73 @@ FamilyTree.prototype.addChild = function(tree) {
   this.children.push(tree);
 };
 
+FamilyTree.prototype.traverse = function(cb, root) {
+  if (root === undefined) {
+    root = true;
+  }
+
+  if (root) {
+    root = false;
+    cb(this);
+  }
+
+  if (this.children) {
+    this.children.forEach(function(child, i, arr) {
+      cb(child, i, arr);
+      
+      child.traverse(cb, root);
+    });
+  } else {
+    return null;
+  }
+
+};
+
 FamilyTree.prototype.find = function(name) {
-  var length = this.children.length;
+  var result = [];
 
-  for (var i = 0; i < length; i++) {
-    var child = this.children[i];
-    if (child.name === name) {
-      return child;
+  this.traverse(function(node) {
+    if (node.name === name) {
+      result = node.name;
+      return;
     }
+  });
 
-    var childsResult = child.find(name);
-    if (childsResult) {
-      return childsResult;
-    };
+  if (result.length) {
+    return result
   }
 
   return null;
+
+};
+
+FamilyTree.prototype.findLoneChildren = function() {
+  var result = [];
+
+  result.push(this.name);
+
+  this.traverse(function(child, i, arr) {
+    if (child.children.length === 1) {
+      result.push(child.children[0].name);
+    }
+  });
+
+  return result;
+  // if (this)
+};
+
+FamilyTree.prototype.findChildlessPeople = function() {
+  var result = [];
+
+  if (this.children.length === 0) {
+    result.push(this.name);
+  }
+
+  this.traverse(function(child, i, arr) {
+    if (child.children.length === 0) {
+      result.push(child.name);
+    }
+  });
+
+  return result;
 };
--- tests.js
+++ tests.js
@@ -7,13 +7,25 @@ var nara = new FamilyTree('Nara');
 var natalie = new FamilyTree('Natalie');
 var oliver = new FamilyTree('Oliver');
 var oscar = new FamilyTree('Oscar');
+var patrick = new FamilyTree('Patrick');
+var robert = new FamilyTree('Robert');
+var roger = new FamilyTree('Roger');
+var ridley = new FamilyTree('Ridley');
+
+
 
 mandy.addChild(natalie);
 mandy.addChild(nara);
 
+natalie.addChild(patrick);
+
 nara.addChild(oliver);
 nara.addChild(oscar);
 
+patrick.addChild(robert);
+patrick.addChild(roger);
+
+roger.addChild(ridley);
 
 describe('the family tree data structure', function() {
 
@@ -28,22 +40,34 @@ describe('the family tree data structure', function() {
     });
 
     it('should allow you to add children', function() {
-      assert(mandy.children.length === 2, 'added a child');
+      assert(mandy.children.length === 2, 'added two children');
     });
 
     it('should let you find a child', function() {
-      expect(mandy.find('Nara')).to.equal(nara);
+      expect(mandy.find('Nara')).to.equal('Nara');
     });
 
-    it('should let you traverse a nodes entire ancestry', function() {
-      mandy.find('Oliver').should.equal(oliver);
+    it('should let you traverse a nodes entire progeny', function() {
+      var result = [];
+      var identity = function(a) {result.push(a.name)};
+      mandy.traverse(function(node) {identity(node)});
+      expect(result.length).to.equal(9);
+
+      expect(mandy.find('Oliver')).to.equal('Oliver');
       expect(mandy.find('Harold')).to.equal(null);
     });
 
   });
 
-  // describe('it should let you find the grandparent of a node', function() {
-  //   expect(mandy.)
-  // });
+  describe('extra functionality', function() { 
+    it('should let you find people with no siblings', function() {
+      assert.deepEqual(mandy.findLoneChildren(), ['Mandy', 'Patrick', 'Ridley']);
+    });
+
+    it('should let you find people with no children', function() {
+      assert.deepEqual(mandy.findChildlessPeople(), ['Robert', 'Ridley', 'Oliver', 'Oscar']);
+    });
+
+  });
 
 });
